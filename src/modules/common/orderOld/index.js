e86912de01569080f5f16dd9a6c1066a3a59c108
DavidJiah <51731944+DavidJiah@users.noreply.github.com>
2020-09-14 10:31:43 +0800

    feat: add order react

---
+++ src/modules/common/orderOld/index.js
@@ -0,0 +1,22 @@
+/*
+ * @Date: 2020-05-29 11:05:46
+ * @LastEditTime: 2020-09-12 17:56:41
+ */
+import React from "react";
+import ReactDom from "react-dom";
+import FastClick from "fastclick";
+
+import App from "./app";
+import { getQueryVariable, setToken, setChannel } from '@/utils';
+import "normalize.css";
+import "@/styles/order/index.less";
+
+FastClick.attach(document.body);
+
+const token = getQueryVariable("token");
+if (token) setToken(token);
+
+const channel = getQueryVariable("channel");
+if (channel) setChannel(channel);
+
+ReactDom.render(<App />, document.getElementById("app"));
